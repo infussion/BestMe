f2cefc035f95fd0a817815c11e77a0ae0a76804f
infussion <infussion@yahoo.com>
2025-05-22 20:33:25 -0400

    Add files via upload

---
+++ BestMe/BestMeTests/BestMeTests.swift
@@ -0,0 +1,17 @@
+//
+//  BestMeTests.swift
+//  BestMeTests
+//
+//  Created by Todd Maiorano on 5/22/25.
+//
+
+import Testing
+@testable import BestMe
+
+struct BestMeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
